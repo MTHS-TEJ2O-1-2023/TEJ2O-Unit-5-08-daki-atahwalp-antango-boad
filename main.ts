64fa6de619e1787a142ef939c14b516ea70c6627
daki-atahwalp-antango-boad <144264104+daki-atahwalp-antango-boad@users.noreply.github.com>
2023-11-22 12:28:16 -0500

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -5,4 +5,34 @@
  * This program motors, turn 90 degrees once within 10cm of an object
 */
 
-basic.showString('Hello, World!')
+// variables
+let distanceFromObject: number = 0
+
+// on start
+basic.showIcon(IconNames.Happy)
+
+// loop forever
+while (true) {
+  if (input.buttonIsPressed(Button.A) == true) {
+  // finding distance from object
+  distanceFromObject = sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+  )
+  basic.showIcon(IconNames.Happy)
+
+  // if within 10cm of an object
+  if (distanceFromObject <= 10)
+      robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B2)
+      robotbit.StepperTurn(robotbit.Steppers.M2, robotbit.Turns.T1B2)
+      robotbit.StpCarMove(10, 48)
+      robotbit.StpCarMove(-10, 48)
+
+    // turning motor 180 degrees
+    basic.showIcon(IconNames.Yes)
+      robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B0)
+      robotbit.StepperTurn(robotbit.Steppers.M2, robotbit.Turns.T1B0)
+      basic.showIcon(IconNames.Happy)
+}
+}
