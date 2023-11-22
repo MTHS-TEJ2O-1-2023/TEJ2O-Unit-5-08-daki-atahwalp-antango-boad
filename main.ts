1a55da241e10d02c6bd3a3e5a65c40d91ccbeb4a
daki-atahwalp-antango-boad <144264104+daki-atahwalp-antango-boad@users.noreply.github.com>
2023-11-22 12:07:29 -0500

    Update pxt.json, main.ts, README.md

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+0/* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Daki A.B
+ * Created on: Nov 2023
+ * This program motors, turn 90 degrees once within 10cm of an object
 */
 
 basic.showString('Hello, World!')
