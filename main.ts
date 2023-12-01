cbee96ad840a796ed898d09a78fd37e73f162f40
daki-atahwalp-antango-boad <144264104+daki-atahwalp-antango-boad@users.noreply.github.com>
2023-12-01 13:02:44 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -13,24 +13,24 @@ basic.showIcon(IconNames.Happy)
 while (true) {
   if (input.buttonIsPressed(Button.A) == true) {
   // finding distance with sonar
-     basic.clearScreen()
-      distanceToObject = sonar.ping(
-          DigitalPin.P1,
-          DigitalPin.P2,
-          PingUnit.Centimeters
-      )
-      basic.showIcon(IconNames.Happy)
-      // if distance is less than 10cm
+    basic.clearScreen()
+    distanceToObject = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+    )
+    basic.showIcon(IconNames.Happy)
+    // if distance is less than 10cm
       if (distanceToObject <= 10) 
         robotbit.StpCarTurn(90, 48, 125)
         robotbit.StpCarMove(10, 48)
 
       } else { 
 
-        // if distance is more than 10cm
-        (distanceToObject > 10) 
-        robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B4)
-        robotbit.StepperTurn(robotbit.Steppers.M2, robotbit.Turns.T1B4)
-        robotbit.StpCarMove(10, 48)
+      // if distance is more than 10cm
+      (distanceToObject > 10) 
+      robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B4)
+      robotbit.StepperTurn(robotbit.Steppers.M2, robotbit.Turns.T1B4)
+      robotbit.StpCarMove(10, 48)
       }
  }
