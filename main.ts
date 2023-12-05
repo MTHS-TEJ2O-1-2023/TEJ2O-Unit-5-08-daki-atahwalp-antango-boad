ca677de287a91397c4ce2d7873fa0847dc433922
daki-atahwalp-antango-boad <144264104+daki-atahwalp-antango-boad@users.noreply.github.com>
2023-12-05 13:24:49 -0500

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -5,36 +5,38 @@
  * This program motors, turn 90 degrees once within 10cm of an object
 */
 
-let distanceToObject: number = 0
+let distanceFromObject: number = 0
 
 // setup
 basic.showIcon(IconNames.Happy)
 
 while (true) {
-  if (input.buttonIsPressed(Button.A) == true) {
-  // finding distance with sonar
+  if (input.buttonIsPressed(Button.A) === true) {
     basic.clearScreen()
-    distanceToObject = sonar.ping(
-        DigitalPin.P1,
-        DigitalPin.P2,
-        PingUnit.Centimeters
-    )
-    basic.showNumber(distanceToObject)
-    basic.showString("cm")
-    basic.showIcon(IconNames.Happy)
+    while (true) {
+        // find distance with sonar
+        distanceFromObject = sonar.ping(
+            DigitalPin.P1,
+            DigitalPin.P2,
+            PingUnit.Centimeters
+        )
+        basic.showNumber(distanceFromObject)
+        basic.pause(500)
+        basic.showIcon(IconNames.Happy)
 
-    // if distance is less than 10cm
-      if (distanceToObject > 10) {
-      robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B4)
-      robotbit.StepperTurn(robotbit.Steppers.M2, robotbit.Turns.T1B4)
-      robotbit.StpCarMove(10, 48)
+        if (distanceFromObject < 10) {
+            robotbit.StpCarMove(-10, 48)
+            basic.pause(500)
+            robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B4)
+            basic.pause(500)
+            robotbit.StepperTurn(robotbit.Steppers.M2, robotbit.Turns.T1B4)
+            basic.pause(500)
+            robotbit.StpCarMove(10, 48)
 
-      } else { 
+        } else {
 
-      // if distance is more than 10cm
-      (distanceToObject <= 10)
-      robotbit.StpCarTurn(90, 48, 125)
-      robotbit.StpCarMove(10, 48)
-      }
-  }
+            robotbit.StpCarMove(10, 48)
+        }
+    }
+}
 }
