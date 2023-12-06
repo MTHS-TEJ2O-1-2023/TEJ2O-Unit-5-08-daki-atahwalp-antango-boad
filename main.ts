2d626088d873550954c7dae86f69f544ae45196f
daki-atahwalp-antango-boad <144264104+daki-atahwalp-antango-boad@users.noreply.github.com>
2023-12-06 13:21:22 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -11,28 +11,29 @@ let distanceFromObject: number = 0
 basic.showIcon(IconNames.Happy)
 
 while (true) {
-  if (input.buttonIsPressed(Button.A) == true) {
+  if (input.buttonIsPressed(Button.A) === true) {
     basic.clearScreen()
     while (true) {
-        // find distance with sonar
-        distanceFromObject = sonar.ping(
-            DigitalPin.P1,
-            DigitalPin.P2,
-            PingUnit.Centimeters
-        )
-        basic.showNumber(distanceFromObject)
-        basic.pause(500)
-        basic.showIcon(IconNames.Happy)
+      // find distance with sonar
+      distanceFromObject = sonar.ping(
+          DigitalPin.P1,
+          DigitalPin.P2,
+          PingUnit.Centimeters
+      )
+      basic.showNumber(distanceFromObject)
+      basic.showString("cm")
+      basic.pause(500)
+      basic.showIcon(IconNames.Happy)
 
-        if (distanceFromObject < 10) {
-            robotbit.StpCarTurn(90, 48, 125)
-            basic.pause(500)
-            robotbit.StpCarMove(10, 48)
+       if (distanceFromObject < 10) {
+          robotbit.StpCarTurn(90, 48, 125)
+          basic.pause(500)
+          robotbit.StpCarMove(10, 48)
 
-        } else {
+      } else {
 
-            robotbit.StpCarMove(10, 48)
-        }
+          robotbit.StpCarMove(10, 48)
+      }
     }
   }
 }
